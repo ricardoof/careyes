968bf7d1b710972aba492bf2e168c2a249f345ef
ricardo ferreira <ricardoferreira4496@gmail.com>
2025-11-30 13:01:57 -0300

    correção user

--- lib/careyes/accounts/user.ex
+++ lib/careyes/accounts/user.ex
@@ -20,16 +20,17 @@ defmodule Careyes.Accounts.User do
     user
     |> cast(attrs, [:usuario, :password, :tipo, :bloqueado, :configuracoes, :instituicao_id])
     |> validate_required([:usuario, :password, :tipo, :instituicao_id])
-    |> put_password_hash()
+    #|> put_password_hash()
   end
 
   # Esta função deve estar definida DENTRO do módulo, mas pode ser privada (defp)
-  defp put_password_hash(changeset) do
-    case get_change(changeset, :password) do
-      password when is_binary(password) ->
-        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
-      _ ->
-        changeset
-    end
-  end
+
+  #defp put_password_hash(changeset) do
+  #  case get_change(changeset, :password) do
+  #    password when is_binary(password) ->
+  #      put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+  #    _ ->
+  #      changeset
+  #  end
+  #end
 end
